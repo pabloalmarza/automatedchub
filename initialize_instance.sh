db129c805f9e43d0aff0d2b8dd3aa43b7d053534
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2021-05-14 13:53:30 +0100

    Update initialize_instance.sh

--- initialize_instance.sh
+++ initialize_instance.sh
@@ -22,7 +22,6 @@ done
   sudo systemctl daemon-reload
   sudo systemctl start docker
   sudo yum -y install git
-  sleep 15
   sudo git clone https://github.com/blackducksoftware/hub.git /home/hub -b $HUB_VERSION
   sudo git clone https://github.com/blackducksoftware/blackduck-alert.git -b $ALERT_VERSION /home/alert
   sudo sed -i  s/SE_ALERT=0/USE_ALERT=1/ /home/hub/docker-swarm/hub-webserver.env
