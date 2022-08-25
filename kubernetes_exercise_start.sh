acd2337964c0a3a6bd883e55360173275c265b9d
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-08-25 12:58:32 +0100

    Update kubernetes_exercise_start.sh

--- kubernetes_exercise_start.sh
+++ kubernetes_exercise_start.sh
@@ -1,4 +1,8 @@
-sudo apt-get -y install helm
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 sudo apt-get -y install jq
 sudo git clone https://github.com/pabloalmarza/kubernetes-hub-exercises.git /home/exercises
 chown -R ubuntu:ubuntu /home/exercises
