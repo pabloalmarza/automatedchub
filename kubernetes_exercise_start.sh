af3f3f771b717cf337a147e1f5331000ee1c6799
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-08-25 12:52:02 +0100

    Update kubernetes_exercise_start.sh

--- kubernetes_exercise_start.sh
+++ kubernetes_exercise_start.sh
@@ -2,3 +2,4 @@ sudo apt-get -y install helm
 sudo apt-get -y install jq
 sudo git clone https://github.com/pabloalmarza/kubernetes-hub-exercises.git /home/exercises
 chown -R ubuntu:ubuntu /home/exercises
+git clone https://github.com/blackducksoftware/hub.git -b v2022.7.0 /home/hub
