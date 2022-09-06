bb46ee444cd5a70d8bb860ea6424f12a5997a452
pabloalmarza <65709372+pabloalmarza@users.noreply.github.com>
2022-09-06 10:28:05 +0100

    Update kubernetes_exercise_start.sh

--- kubernetes_exercise_start.sh
+++ kubernetes_exercise_start.sh
@@ -9,6 +9,7 @@ sudo chown -R ubuntu:ubuntu /home/exercises
 git clone https://github.com/blackducksoftware/hub.git -b v2022.7.0 /home/hub
 sudo chown -R ubuntu:ubuntu /home/hub
 cp /tmp/kube_controller.pem /home/exercises/certs/
+sudo chown ubuntu:ubuntu /home/exercises/certs/kube_controller.pem > /dev/null 2>&1
 sudo chmod 400 /home/exercises/certs/kube_controller.pem > /dev/null 2>&1
 chmod 777 /home/exercises/deploy
 chmod 777 /home/exercises/delete
